87f879735ca7fbaa2a0ffacd9652619525e1b139
José Leyva <jrleyva41096@gmail.com>
2022-11-02 18:36:22 -0400

    Added questions for the add role question with placer choices

--- index.js
+++ index.js
@@ -5,25 +5,57 @@ const mysql = require('mysql2');
 // Initial menu 
 function mainMenu() {
     return inquirer
-      .prompt([{
-        name: 'mainMenu',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: ['View all Employees', 'Add Employee', 'Update Employee role', 'View all roles', 'Add roles', 'View all Departments', 'Add Department', 'Quit'],
-      }])
-      .then((data) => { return data })
-  }
+        .prompt([{
+            name: 'mainMenu',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View all Employees', 'Add Employee', 'Update Employee role', 'View all roles', 'Add roles', 'View all Departments', 'Add Department', 'Quit'],
+        }])
+        .then((data) => { return data })
+}
 
 //   mainMenu()
-  
+// Adds new departnment
 function addDept(params) {
     return inquirer
-    .prompt([{
-        name: 'department',
-        type: 'input',
-        message: 'What is the name of the Department?'
-    }])
-    .then((data) => { return data })
+        .prompt(
+            [
+                {
+                    name: 'department',
+                    type: 'input',
+                    message: 'What is the name of the Department?'
+                }
+            ]
+        )
+        .then((data) => { return data })
 }
 
 // addDept()
+// Adds new Role
+function addRole(params) {
+    return inquirer
+        .prompt(
+            [
+                {
+                    name: 'role',
+                    type: 'input',
+                    message: 'What is the name of the Role?'
+                },
+                {
+                    name: 'roleSalary',
+                    type: 'input',
+                    message: 'What is the salary of the role?'
+                },
+                {
+                    name: 'roleDept',
+                    type: 'list',
+                    message: 'Which department does the role belong to?',
+                    choices: ['Deez Nuts', 'Cheeked Up', 'Bruh', '']
+                },
+                
+            ]
+        )
+        .then((data) => { return data })
+}
+
+addRole()
